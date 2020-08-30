b8190879a3f74b1be24b669f4e6c5601f2b42db8
xunmi <xunmi1@outlook.com>
2020-08-30 13:34:47 +0800

    fix: fix options merge

--- src/middleware/fetch.middleware.ts
+++ src/middleware/fetch.middleware.ts
@@ -1,12 +1,5 @@
 import { Next, Context } from '../interfaces';
-import { isPlainJSON } from '../utils';
-
-const METHODS_NO_BODY = ['GET', 'HEAD'];
-const canHaveBody = (method?: string) => !!method && !METHODS_NO_BODY.includes(method);
-
-const setHeaderIfUnset = (headers: Headers, name: string, value: string) => {
-  if (!headers.has(name)) headers.append(name, value);
-};
+import { isPlainJSON, setIfNull } from '../utils';
 
 const transformParams = (url: string, baseURL?: string, params?: URLSearchParams) => {
   const result = new URL(url, baseURL);
@@ -21,11 +14,11 @@ export const fetchMiddleware = <T>(ctx: Context<T>, next: Next) => {
   const { href } = transformParams(url, baseURL, params);
 
   // default common headers
-  setHeaderIfUnset(options.headers, 'Accept', 'application/json, text/plain, */*');
+  setIfNull(options.headers, 'Accept', 'application/json, text/plain, */*');
 
-  if (options.body == null && data != null && canHaveBody(options.method)) {
+  if (options.body == null && data != null) {
     if (isPlainJSON(data)) {
-      setHeaderIfUnset(options.headers, 'Content-Type', 'application/json;charset=UTF-8');
+      setIfNull(options.headers, 'Content-Type', 'application/json;charset=UTF-8');
       options.body = JSON.stringify(data);
     }
     options.body = data;
--- src/utils.ts
+++ src/utils.ts
@@ -2,15 +2,18 @@ import { RequestOptions } from './interfaces';
 
 const toRawType = (val: unknown) => Object.prototype.toString.call(val).slice(8, -1);
 
-export const isPlainJSON = (val: unknown): val is Record<any, any> => {
+export const isPlainJSON = (val: unknown): val is Record<any, any> | any[] => {
   const type = toRawType(val);
   return ['Object', 'Array'].includes(type);
 };
 
-export const isObject = (val: unknown): val is Record<any, any> => val !== null && typeof val === 'object';
 export const isNumber = (val: unknown): val is number => typeof val === 'number';
 export const isString = (val: unknown): val is string => typeof val === 'string';
 export const isFunction = (val: unknown): val is Function => typeof val === 'function';
+
+export const setIfNull = (target: Headers | URLSearchParams, key: string, value: string) => {
+  if (!target.has(key)) target.set(key, value);
+};
 
 export const mergeOptions = (global: RequestOptions = {}, options: RequestOptions = {}): RequestOptions => {
   const merged = { ...global, ...options };
@@ -33,16 +36,17 @@ const mergeHeaders = (val1: RequestHeaders, val2: RequestHeaders) => {
   const source = new Headers(val1);
   const result = new Headers(val2);
   // For options, directly overwrite, and not append
-  source.forEach((v, k) => result.set(k, v));
+  source.forEach((v, k) => setIfNull(result, k, v));
   return result;
 };
 
 const mergeParams = (val1: RequestParams, val2: RequestParams) => {
   const source = new URLSearchParams(val1);
   const result = new URLSearchParams(val2);
-  source.forEach((v, k) => result.set(k, v));
+  source.forEach((v, k) => setIfNull(result, k, v));
   return result;
 };
 
 const ABORT_ERROR_NAME = 'AbortError';
-export const isAborted = (val: unknown): val is DOMException => val instanceof DOMException && val.name === ABORT_ERROR_NAME;
+export const isAborted = (val: unknown): val is DOMException =>
+  val instanceof DOMException && val.name === ABORT_ERROR_NAME;
