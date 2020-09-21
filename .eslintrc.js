40778c8472126529ad8666b4dd847e7203329cfb
xunmi <xunmi1@outlook.com>
2020-09-21 15:06:19 +0800

    style: use eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+const prettierConfig = require('@xunmi/prettier-config');
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // Uses `eslint-config-prettier` to disable ESLint rules from `@typescript-eslint/eslint-plugin`
+    'prettier/@typescript-eslint',
+    // Must always be last
+    "plugin:prettier/recommended",
+  ],
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    "@typescript-eslint/ban-ts-comment": 'off',
+
+    'prettier/prettier': ['warn', prettierConfig],
+  },
+};
